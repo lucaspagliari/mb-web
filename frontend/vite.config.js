50845470311fc696dbda7518f96f630ffeaa31bb
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-07-02 16:29:59 -0300

    add vite plugins and favicon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,29 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import ViteWebfontDownload from 'vite-plugin-webfont-dl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-
+    Components({ 
+      dts: 'src/components.d.ts',
+      dirs: ['src/components']
+    }),
+    ViteWebfontDownload(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@/assets/stylesheets/main.scss'
+      }
     }
   }
 })
