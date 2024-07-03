368b442e7110a9f0e7e025ce3763959363409f82
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-07-03 01:44:25 -0300

    feat: add BRadioGroup component

---
+++ frontend/src/components/BRadioGroup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="v-radio-group">
+    <b-radio v-for="{ label } in items" :key="label" :name="name">{{ label }}</b-radio>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array
+  }
+})
+
+import { getUid } from '@/utils'
+
+const name = getUid()
+</script>
+
+<style lang="scss" scoped>
+.v-radio-group {
+  display: flex;
+  justify-content: space-between;
+  margin: 12px 0;
+}
+</style>
