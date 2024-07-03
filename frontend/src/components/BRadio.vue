0e75f368ca4d5915cf1e14b1e666ef07a5edbc3c
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-07-03 01:12:23 -0300

    feat: add BRadio component

---
+++ frontend/src/components/BRadio.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="b-radio">
+    <input class="b-radio__input" :id="id" :name="name" type="radio" />
+    <label class="b-radio__label" :for="id">
+      <slot name="default"></slot>
+    </label>
+  </div>
+</template>
+
+<script setup>
+import { getUid } from '@/utils'
+
+const props = defineProps({
+  label: {
+    type: String
+  },
+  name: {
+    type: String,
+    default: getUid()
+  }
+})
+const id =  getUid()
+</script>
+
+<style lang="scss" scoped>
+.b-radio {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  accent-color: $black;
+
+  &__label {
+    padding-left: 0.25rem;
+  }
+
+  &__input {
+    height: 1.125rem;
+    border-radius: 0.5rem;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ frontend/src/components/__tests__/BRadio.test.js
@@ -0,0 +1,15 @@
+import BRadio from "../BRadio.vue";
+import { describe, it, expect } from "vitest";
+import { faker } from "@faker-js/faker";
+import { mount } from "@vue/test-utils"
+
+const radioFactory = (props, options) => mount(BRadio, { props, ...options })
+
+describe("Component BRadio", () => {
+    it("should render text passed in slot", () => {
+        const text = faker.string.alphanumeric(16);
+        const radio = radioFactory({}, { slots: { default: text } })
+
+        expect(radio.text()).toBe(text)
+    });
+});
