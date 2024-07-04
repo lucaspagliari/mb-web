61c5b7b5c3d235b3bc4eb8b0532f9ce258238c45
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-07-04 16:56:20 -0300

    feat: add client password step

---
+++ frontend/src/components/ClientPasswordForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-field
+      v-model="form.password"
+      v-model:is-valid="formValid.password"
+      label="Sua senha"
+      type="password"
+      :rules="[required]"
+    ></b-field>
+  </div>
+</template>
+
+<script setup>
+import { required } from '@/utils/rules'
+import { reactive, watch, inject } from 'vue'
+
+const form = inject('form')
+
+const model = defineModel()
+const formValid = reactive({})
+watch(formValid, () => {
+  model.value = Object.keys(formValid).every((k) => formValid[k])
+})
+</script>
+
+<style lang="scss" scoped></style>
