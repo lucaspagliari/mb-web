354c191a036354069d345c006d97126db29a5ef9
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-07-04 16:56:32 -0300

    feat: add client review step

---
+++ frontend/src/components/ClientReviewForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-field v-model="form.email" label="Endereço de e-mail" type="email" disabled></b-field>
+    <b-field v-model="form.name" label="Nome" disabled></b-field>
+    <b-field v-model="form.document" disabled></b-field>
+    <b-field v-model="form.date" type="date" disabled></b-field>
+    <b-field v-model="form.phone" label="Telefone" disabled></b-field>
+    <b-field v-model="form.password" label="Senha" type="password" disabled></b-field>
+  </div>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+
+const form = inject('form')
+</script>
+
+<style lang="scss" scoped></style>
