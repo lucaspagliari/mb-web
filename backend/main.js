b8be9648d65a7e94269a07fb82d69732db18f99d
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-07-04 17:00:28 -0300

    feat: base for api with express

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,27 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
 const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
+
+const directory = '../frontend/dist/';
+
+app.use(express.static(directory));
+
+app.get('/', (req, res) => {
+    res.redirect('/register')
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(directory + "index.html");
+})
+
+app.post('/register', (req, res) => {
+    res.status(201).json({ message: 'created' })
+})
 
 app.listen(PORT, () => {
     console.log(`App running on ${PORT}`);
