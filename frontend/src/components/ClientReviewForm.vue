0534c19e2813c42d375cb1f7cbfa6a324f16ce87
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-07-04 21:10:20 -0300

    fix: change label based on isCompany

--- frontend/src/components/ClientReviewForm.vue
+++ frontend/src/components/ClientReviewForm.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
-    <b-field v-model="form.email" label="Endereço de e-mail" type="email" disabled></b-field>
+    <b-field
+      v-model="form.email"
+      label="Endereço de e-mail"
+      type="email"
+      disabled
+    ></b-field>
     <b-field v-model="form.name" label="Nome" disabled></b-field>
-    <b-field v-model="form.document" disabled></b-field>
-    <b-field v-model="form.date" type="date" disabled></b-field>
+    <b-field v-model="form.document" :label="documentLabel" disabled></b-field>
+    <b-field v-model="form.date" :label="dateLabel" type="date" disabled></b-field>
     <b-field v-model="form.phone" label="Telefone" disabled></b-field>
     <b-field v-model="form.password" label="Senha" type="password" disabled></b-field>
   </div>
 </template>
 
 <script setup>
-import { inject } from 'vue'
+import { inject, computed } from 'vue'
 
 const form = inject('form')
+
+const documentLabel = computed(() => (form.isCompany ? 'CPNJ' : 'CPF'))
+const dateLabel = computed(() =>
+  form.isCompany ? 'Data de abertura' : 'Data de nascimento'
+)
 </script>
 
 <style lang="scss" scoped></style>
