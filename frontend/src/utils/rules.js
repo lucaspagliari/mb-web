6b8d29d271eb1bdb6b42bb4fa905820685bba67d
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-07-04 21:07:40 -0300

    feat: add rule for date comparison

--- frontend/src/utils/rules.js
+++ frontend/src/utils/rules.js
@@ -1,5 +1,5 @@
 import { cpf, cnpj } from 'cpf-cnpj-validator';
-import phone from "phone"
+import { phone } from "phone"
 
 export const required = (v) => !!v || 'Campo obrigatório'
 
@@ -11,9 +11,13 @@ export const isCpf = (v) => !!cpf.isValid(v) || 'CPF inválido'
 
 export const isCnpj = (v) => !!cnpj.isValid(v) || 'CNPJ inválido'
 
-export const isPhoneNumber = (v) => {
+export const isPhoneNumber = (v) => phone(v, { country: "BR", validateMobilePrefix: false }).isValid || 'Número de telefone inválido'
 
-    console.log(phone(v, { country: "BR", validateMobilePrefix: false }));
+export const dateBefore = (before) => {
+  return (v) => {
+    const date = new Date(v)
+    return before - date > 0 || `Data deve ser anterior ao dia ${before.toLocaleDateString('pt-br')}`
+  }
+}
 
-    return phone(v, { country: "BR", validateMobilePrefix: false }).isValid || 'Número de telefone inválido'
-    }
+export const dateBeforeNow = dateBefore(new Date())
